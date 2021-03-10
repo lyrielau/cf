a3e99341b0911c8f6fc60019963787df0ac0f076
simon <lyrielau@gmail.com>
2021-03-10 23:41:54 +0800

    Update cf-openwrt.sh

--- cf-openwrt.sh
+++ cf-openwrt.sh
@@ -386,6 +386,7 @@ done
 	echo 峰值速度 $max kB/s
 	echo 数据中心 $colo
 	echo 总计用时 $((end_seconds-start_seconds)) 秒
+	
 	iptables -t nat -D OUTPUT $(iptables -t nat -nL OUTPUT --line-number | grep $localport | awk '{print $1}')
 	iptables -t nat -A OUTPUT -p tcp --dport $localport -j DNAT --to-destination $anycast:$remoteport
 	echo $(date +'%Y-%m-%d %H:%M:%S') IP指向 $anycast>>/usr/dns/cfnat.txt
